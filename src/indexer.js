137f2e9788784bebc16002bb02090dd99b83c028
Mibble <mibble@outlook.com.au>
2024-04-05 01:36:43 +1030

    Removed unused indexer script

--- src/indexer.js
+++
@@ -1,8 +0,0 @@
-import env from './env.js';
-import Index from './docs/index.js';
-
-console.log('Begin index...');
-const index = new Index(env.DOCS_ROOT);
-index.indexDirectory();
-console.log('Done');
-
